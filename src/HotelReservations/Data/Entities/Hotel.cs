8da62656b04f5b012a92b65837a4e786305b083d
Victor147 <viktorivanov_e18@schoolmath.eu>
2023-01-31 08:34:31 +0200

    Made hotel entity changes to suit the hotel card.

--- src/HotelReservations/Data/Entities/Hotel.cs
+++ src/HotelReservations/Data/Entities/Hotel.cs
@@ -2,6 +2,10 @@
 
 public class Hotel : Entity<int>
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string Location { get; set; }
     public IEnumerable<Reservation> Reservations { get; set; }
     public IEnumerable<Room> Rooms { get; set; }
     public IEnumerable<User> Employees { get; set; }
